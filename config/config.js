59499e032072fcba68d6799ab0512d2a7981b695
Dinty1 <dintybmrt@gmail.com>
2022-03-07 22:12:56 +0000

    Change colour

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 export default {
     "prefix": "-",
     "owner": "716779626759716914",
-    "embedColor": "#e67e22",
+    "embedColor": "#64acd7",
     "supportServerInviteLink": "https://discord.gg/AKPkZwR",
     /*"checkmark": "<:authorized:585790083161128980> ",
     "xmark": "<:xmark:314349398824058880> "*/
