2976d679fa72d0a18cac7676d56bde050ce084f2
Dinty1 <dintybmrt@gmail.com>
2023-07-04 21:45:14 +0100

    Show prefix in status

--- config/config.js
+++ config/config.js
@@ -1,8 +1,19 @@
-export default {
+export const devConfig = {
+    "prefix": ">>",
+    "owner": "716779626759716914",
+    "embedColor": "#64acd7",
+    "supportServerInviteLink": "https://discord.gg/BCVHCzd77R",
+    /*"checkmark": "<:authorized:585790083161128980> ",
+    "xmark": "<:xmark:314349398824058880> "*/
+    "checkmark": ":white_check_mark: ",
+    "xmark": ":x: "
+}
+
+export const prodConfig = {
     "prefix": "-",
     "owner": "716779626759716914",
     "embedColor": "#64acd7",
-    "supportServerInviteLink": "https://discord.gg/AKPkZwR",
+    "supportServerInviteLink": "https://discord.gg/BCVHCzd77R",
     /*"checkmark": "<:authorized:585790083161128980> ",
     "xmark": "<:xmark:314349398824058880> "*/
     "checkmark": ":white_check_mark: ",
